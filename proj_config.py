311d23a40daf4ed51d35c798888d44a4bb220480
Orthel Toralen <orthel.toralen@gmail.com>
2025-07-08 00:39:08 -0400

    added goggle sheets URL

--- proj_config.py
+++ proj_config.py
@@ -4,3 +4,5 @@ sde_path = "sde_info.db"
 sde_url = f"sqlite+libsql:///{sde_path}"
 
 gopgle_private_key_file = "wcdoctrines-1f629d861c2f.json"
+google_sheet_url = "https://docs.google.com/spreadsheets/d/1frGs3XzB7kooVoN-rqRUfoYX3k3FIFgo1ZDAypzc-pI/edit?gid=1897807024#gid=1897807024"
+sheet_name = "raw_mkt_data"
